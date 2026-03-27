94c7a43bc9a96168fdfc56deb078c997afe17663
rakesh-techie09 <rakeshjadennavar@gmail.com>
2026-03-27 10:02:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ package practice1;
 
 public class HelloWorld {  
 	public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello bacchon!");
 }
 
 }
